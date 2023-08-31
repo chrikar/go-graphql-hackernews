919f1c0ad096e92aac1c61589c56974ec4f13143
Christos Kargas <kargas.c@gmail.com>
2023-08-31 16:03:40 +0300

    feat: add steps up to database setup
    - includes migrations

---
+++ server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+
+	hackernews "github.com/chrikar/hackernews/graph"
+	database "github.com/chrikar/hackernews/internal/pkg/db/mysql"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	router := chi.NewRouter()
+
+	database.InitDB()
+	defer database.CloseDB()
+	database.Migrate()
+	server := handler.NewDefaultServer(hackernews.NewExecutableSchema(hackernews.Config{Resolvers: &hackernews.Resolver{}}))
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", server)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
